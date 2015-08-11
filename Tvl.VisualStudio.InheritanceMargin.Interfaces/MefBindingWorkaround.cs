14c519c93c5cf933df2b5b017279132c03c2a149
Sam Harwell <sam@tunnelvisionlabs.com>
2015-08-10 19:36:38 -0500

    Add a dummy export to Tvl.VisualStudio.InheritanceMargin.Interfaces
    Fixes #6

---
+++ Tvl.VisualStudio.InheritanceMargin.Interfaces/MefBindingWorkaround.cs
@@ -0,0 +1,17 @@
+﻿namespace Tvl.VisualStudio.InheritanceMargin.Interfaces
+{
+    using System.ComponentModel.Composition;
+
+    /// <summary>
+    /// This class exists for the sole purpose of working around issue
+    /// https://github.com/tunnelvisionlabs/InheritanceMargin/issues/6.
+    /// </summary>
+    /// <remarks>
+    /// <para>Applying the <see cref="ExportAttribute"/> is required for the affected version(s) of Visual Studio to
+    /// place this interfaces assembly in the MEF cache.</para>
+    /// </remarks>
+    [Export]
+    internal class MefBindingWorkaround
+    {
+    }
+}
