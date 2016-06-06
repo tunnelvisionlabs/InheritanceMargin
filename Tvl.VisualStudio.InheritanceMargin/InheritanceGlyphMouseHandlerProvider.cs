258089c2b838e6e76f0728fa789282b6a2bc4ca9
Sam Harwell <sam@tunnelvisionlabs.com>
2016-06-06 09:32:31 -0500

    Switch to the MIT license

--- Tvl.VisualStudio.InheritanceMargin/InheritanceGlyphMouseHandlerProvider.cs
+++ Tvl.VisualStudio.InheritanceMargin/InheritanceGlyphMouseHandlerProvider.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
-// Licensed under the Microsoft Reciprocal License (MS-RL). See LICENSE in the project root for license information.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 namespace Tvl.VisualStudio.InheritanceMargin
 {
